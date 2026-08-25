8b3f131c45eed26a5dcc988e491b4f8b705cbf8a
crs17a <crs17a@gmail.com>
2026-08-25 13:59:21 +0200

    refactor: improve SCSS structure and readability, update version in asset file

--- build/index.asset.php
+++ build/index.asset.php
@@ -2,4 +2,4 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '807e088c3a7b4d0bd403');
+return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '02f3f0615699883de21f');
